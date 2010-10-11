4908b05e9584549496dcd005c37feeca9fefd0ef
MJ Hofland <memikem@hotmail.com>
2010-10-11 20:20:44 +0200

    2e commit

--- dashboard.inc.php
+++ dashboard.inc.php
@@ -8,12 +8,12 @@
  * 
  * @author GP Sietzema, Sterc.nl
  */
-$_lang['active_users_none'] = 'Er zijn geen active gebruikers.';
+$_lang['active_users_none'] = 'Er zijn geen actieve gebruikers.';
 $_lang['add_user'] = 'Nieuwe Gebruiker';
 $_lang['create_resource'] = 'Nieuwe Resource';
 $_lang['dashboard'] = 'Dashboard';
 $_lang['documentation'] = 'Documentatie';
-$_lang['file_bug'] = 'File a bug';
+$_lang['file_bug'] = 'Rapporteer een fout';
 $_lang['forums'] = 'Forums';
 $_lang['help'] = 'Help';
 $_lang['modx_news'] = 'MODx Nieuws';
@@ -21,7 +21,7 @@ $_lang['onlineusers_action'] = 'Actie';
 $_lang['onlineusers_actionid'] = 'Actie-ID';
 $_lang['onlineusers_ipaddress'] = 'Gebruiker\'s IP adres';
 $_lang['onlineusers_lasthit'] = 'Laatste Hit';
-$_lang['onlineusers_message'] = 'This list shows all users active within the last 20 minutes (current time is [[+curtime]])';
+$_lang['onlineusers_message'] = 'In deze lijst staan alle actieve gebruikers uit de laatste 20 minuten (het is nu [[+curtime]])';
 $_lang['onlineusers_title'] = 'Online gebruikers';
 $_lang['onlineusers_user'] = 'Gebruiker';
 $_lang['onlineusers_userid'] = 'Gebruiker\'s ID';
@@ -30,8 +30,8 @@ $_lang['security_notices'] = 'Beveiligings opmerkingen';
 $_lang['view_tree'] = 'Bekijk Tree';
 $_lang['welcome_back'] = 'Welkom terug, [[+name]].';
 $_lang['welcome_messages'] = 'Er zijn <strong>%d</strong> berichten in je inbox, waarvan <strong>%s</strong> ongelezen.';
-$_lang['welcome_title'] = 'Welcome to your MODx Content Manager';
-$_lang['yourinfo_message'] = 'This section shows some information about you:';
+$_lang['welcome_title'] = 'Welkom bij de MODx Content Manager';
+$_lang['yourinfo_message'] = 'Deze sectie laat informatie over jou zien:';
 $_lang['yourinfo_previous_login'] = 'Je laatste login:';
 $_lang['yourinfo_title'] = 'jouw info';
 $_lang['yourinfo_total_logins'] = 'Totaal aantal logins:';
